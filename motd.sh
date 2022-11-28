71eca7c298fc8f13660178b1382be7b640c55a92
MiguelTheDeveloper <87907496+MiguelTheDeveloper@users.noreply.github.com>
2022-11-28 10:21:36 +0000

    tisalabs.motd

---
+++ motd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "
+                                                                                   
+$(tput setaf 5)        //                                                                     
+$(tput setaf 5)     ./   $(tput setaf 6)    ,(////////(*                                                    
+$(tput setaf 5)    /.   $(tput setaf 6)  .///////////////\\\\\\                                                
+$(tput setaf 5)   /,   $(tput setaf 6)  ///////   .//////*  *                                               
+$(tput setaf 5)  */    $(tput setaf 6)  /// $(tput sgr0) __      $(tput setaf 6)  ///*                                                 
+$(tput setaf 5)  //      $(tput sgr0)    /  \   $(tput setaf 6)      //                                                 
+$(tput setaf 5)  *//      $(tput sgr0)   \__/      $(tput setaf 6)   */.                                                
+$(tput setaf 5)   ////           ,///  $(tput setaf 6)   ./                                                 
+$(tput setaf 5)    ///////    ///////  $(tput setaf 6)   //                                                 
+$(tput setaf 5)  //////////////////   $(tput setaf 6)   (/                                                  
+$(tput setaf 5)      ///////////.     $(tput setaf 6) ./                                                    
+$(tput setaf 5)                     $(tput setaf 6) /,                                                      
+$(tput sgr0)
+  _______ _           _           _         
+ |__   __(_)         | |         | |        
+    | |   _ ___  __ _| |     __ _| |__  ___ 
+    | |  | / __|/ _\` | |    / _\` | '_ \/ __|
+    | |  | \__ \ (_| | |___| (_| | |_) \__ \\
+    |_|  |_|___/\__,_|______\__,_|_.__/|___/
+                                            
+$(tput sgr0)"
